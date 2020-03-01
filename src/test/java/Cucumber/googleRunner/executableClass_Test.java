e6860f372d2014ce9e85a5b0b1c3af6ab08ec162
sumon11218 <ksmsumon@gmail.com>
2020-03-01 11:56:54 -0500

    second commit

--- src/test/java/Cucumber/googleRunner/executableClass_Test.java
+++ src/test/java/Cucumber/googleRunner/executableClass_Test.java
@@ -7,8 +7,9 @@ import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-        features = {"src/test/java/Cucumber/Features/google.feature"},
-        glue={"Cucumber.POM_StepDefinitions"},
+        //new changes
+        features = {"src/test/java/Cucumber/Features"},
+        glue={"Cucumber.StepDefinitions"},
         plugin = {"com.cucumber.listener.ExtentCucumberFormatter:target/html/ExtentReport.html"}
 )//end of your feature path
 public class executableClass_Test {
